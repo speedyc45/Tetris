0327018bf895b4672d672546a381878cd0591125
Callum Kipin <callum.kipin@student.tdsb.on.ca>
2018-10-02 20:53:58 -0400

    Added main Tetris.java file
    -Main method also added (currently empty)
    -Basic comments started

---
+++ src/Tetris.java
@@ -0,0 +1,17 @@
+/* 
+ *
+ *
+ * Written by: Callum Kipin
+ */
+
+//import statements
+
+
+//main Tetris class
+public class Tetris {
+
+    public static main (String[] args) {
+
+
+    } //end of main method
+} //end of Tetris class
