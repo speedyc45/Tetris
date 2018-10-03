9345b9da509d46afc061b44d16c8aa2de256a640
speedyc45 <callum.kipin@student.tdsb.on.ca>
2018-10-03 10:08:07 -0400

    Added Blcok and Tetrimino classes
    -Bare bones (add fucntionality!)

---
+++ src/Block.java
@@ -0,0 +1,25 @@
+//
+
+//
+public class Block {
+    //
+    int colour;
+    boolean partOfShape;
+
+    //
+    public Block() {
+        colour = 0;
+        partOfShape = false;
+    } //end of Block() constructor
+
+    //
+    public Block(int colour) {
+        this.colour = colour;
+        partOfShape = false;
+    } //end of Block(int colour) constructor
+
+    //destroy the block
+    public void destroy() {
+        //TODO
+    }
+} //end of Block class
---
+++ src/Tetrimino.java
@@ -0,0 +1,13 @@
+//
+
+//
+public class Tetrimino {
+    //initialize the necessary variables
+    Block[] blocks;
+    char[] shape;
+
+    //
+    public Tetrimino() {
+
+    }
+} //end of Tetrimino class
